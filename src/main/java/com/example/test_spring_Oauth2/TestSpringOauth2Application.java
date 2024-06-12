71509a513166ce3096c1e1c294234971a1708314
irwinekabakti <irwinekabakti@gmail.com>
2024-06-12 22:50:27 +0700

    initial commit

---
+++ src/main/java/com/example/test_spring_Oauth2/TestSpringOauth2Application.java
@@ -0,0 +1,13 @@
+package com.example.test_spring_Oauth2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestSpringOauth2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestSpringOauth2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/test_spring_Oauth2/TestSpringOauth2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.test_spring_Oauth2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestSpringOauth2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
